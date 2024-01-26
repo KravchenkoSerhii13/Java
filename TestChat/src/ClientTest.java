3f6e07e31ebeda41cfc5991b1dc81b57465102c2
KravchenkoSerhii13 <157769564+KravchenkoSerhii13@users.noreply.github.com>
2024-01-26 14:24:32 +0200

    Update ClientTest.java

--- TestChat/src/ClientTest.java
+++ TestChat/src/ClientTest.java
@@ -17,7 +17,7 @@ public class ClientTest {
 
     public static void main(String[] args) {
         try {
-            Socket socket = new Socket("25.57.221.253", 59001);
+            Socket socket = new Socket("localhost", 59001);
             System.out.println("Connected to server.");
 
             outputStream = new ObjectOutputStream(socket.getOutputStream());
